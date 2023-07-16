24bec26431a2b13337d62d23ae06774ecf93d876
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-07-16 18:04:20 +0530

    middleware, nodemon

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,33 @@
 import express from 'express';
+import router from './router';
+import morgan from 'morgan';
+import cors from 'cors';
+import { protect } from './modules/auth';
+import { createNewUser, signIn } from './handlers/user';
+
 const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extends: true }));
+
+/* MiddleWare */
+// app.use((req, res, next) => {
+//   req.secret__thing = 'Top Secret';
+//   res.status(401);
+//   res.send('Nope');
+//   // next();
+// });
 
 app.get('/', (req, res) => {
   console.log('Hello from Express Server');
   res.status(200);
   res.json({ message: 'Hello' });
 });
+
+app.use('/api', protect, router);
+app.post('/user', createNewUser);
+app.post('/signin', signIn);
 
 export default app;
