6655aa4877381397ed6c74bb57ddfddef87d75ac
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-07-16 18:05:22 +0530

    authentication

---
+++ src/modules/auth.ts
@@ -0,0 +1,46 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+export const comparePasswords = (password, hashedPassword) => {
+  return bcrypt.compare(password, hashedPassword);
+};
+
+export const hashPassword = (password) => {
+  return bcrypt.hash(password, 5);
+};
+
+export const createJWT = (user) => {
+  const token = jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.JWT_SECRET
+  );
+  return token;
+};
+
+export const protect = (req, res, next) => {
+  const bearer = req.headers.authorization;
+  if (!bearer) {
+    res.status(401);
+    res.json({ message: 'not autherized' });
+    return;
+  }
+
+  const [, token] = bearer.split(' ');
+
+  if (!token) {
+    res.status(401);
+    res.json({ message: 'not autherized' });
+    return;
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (e) {
+    console.error(e);
+    res.status(401);
+    res.send({ message: 'invalid token' });
+    return;
+  }
+};
